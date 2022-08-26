109327f0e267d20d7a82995e5c2e911d9775364e
kenji-haya <kenji_hayashi@v-kki.co.jp>
2022-08-26 17:20:15 +0900

    統一コード追加

--- kki_product/models/product.py
+++ kki_product/models/product.py
@@ -8,6 +8,7 @@ class kki_product(models.Model):
 
     name2 = fields.Char()
     marge_name = fields.Char(compute='_get_marge_name', store=True)
+    unified_id = fields.Char('unified_id')
 
     @api.depends('name', 'name2')
     def _get_marge_name(self):
