62400f2a774f9f053788ad27b69856275e5235b1
kenji-haya <kenji_hayashi@v-kki.co.jp>
2022-08-29 15:15:40 +0900

    20220829

--- kki_product/__manifest__.py
+++ kki_product/__manifest__.py
@@ -24,9 +24,10 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/product.xml',
+        'views/product_category.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- kki_product/models/product.py
+++ kki_product/models/product.py
@@ -8,6 +8,8 @@ class kki_product(models.Model):
 
     name2 = fields.Char()
     marge_name = fields.Char(compute='_get_marge_name', store=True)
+    unified_id = fields.Char('unified_id')
+    product_category = fields.Many2one('kki_product.categoryid', string='product_category')
 
     @api.depends('name', 'name2')
     def _get_marge_name(self):
